d8949e7b998b9891cc2bf3d226456031a9b032a4
Ryan <ryc9011@users.noreply.github.com>
2016-08-03 18:13:16 -0700

    Add files via upload
    v1

---
+++ config.bat
@@ -0,0 +1,3 @@
+set npath=Replace this with the path to Necrobot.exe, no quotes
+set ppath=Replace this with the path to PogoLocationFeeder.GUI.exe, no quotes
+set qpath=Replace this with the path to QuickSpin.exe, no quotes
