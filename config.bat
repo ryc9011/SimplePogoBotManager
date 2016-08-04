f82bc45821984997134b44f15ad172fb3911b889
Ryan <ryc9011@users.noreply.github.com>
2016-08-03 17:43:10 -0700

    Create config.bat

---
+++ config.bat
@@ -0,0 +1,3 @@
+set npath=Replace this with the path to Necrobot.exe, without quotes
+set ppath=Replace this with the path to PogoLocationFeeder.GUI.exe, without quotes
+set qpath=Replace this with the path to QuickSpin.exe, without quotes
