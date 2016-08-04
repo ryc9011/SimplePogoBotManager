5ece20d84666f5b0ef519dac7110529de9a28d07
Ryan <ryc9011@users.noreply.github.com>
2016-08-03 18:16:54 -0700

    Delete config.bat

--- config.bat
+++
@@ -1,3 +0,0 @@
-set npath=Replace this with the path to Necrobot.exe, no quotes
-set ppath=Replace this with the path to PogoLocationFeeder.GUI.exe, no quotes
-set qpath=Replace this with the path to QuickSpin.exe, no quotes
