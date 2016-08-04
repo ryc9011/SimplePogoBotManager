fd3f66981b7db5f66539e19694a507a5d3e387bc
Ryan <ryc9011@users.noreply.github.com>
2016-08-03 23:38:43 -0700

    New config.bat for v3

--- config.bat
+++ config.bat
@@ -1,7 +1,34 @@
-set npath=[Replace this with the FOLDER (directory) containing Necrobot.exe. No directories with spaces. No quotes. No brackets.]
-set npath2=[Same format. Only fill this out if you have a second account]
-set npath3=[Same format. Only fill this out if you have a third account]
-set npath4=[Same format. Only fill this out if you have a fourth account]
-set npath5=[Same format. Only fill this out if you have a fifth account]
-set ppath=[Replace this with the FOLDER (directory) containing PogoLocationFeeder.GUI.exe. No directories with spaces. No quotes. No brackets.]
-set qpath=[Replace this with the FOLDER (directory) containing QuickSpin.exe. No directories with spaces. No quotes. No brackets.]
+: Fill out the following config file by inputting your settings
+: Do NOT use quotes, Do not put a space at the beginning
+: This program currently does NOT support folders/directories with spaces in them
+: For example, C:\Users\Bob\Release is acceptable
+: and C:\Users\Bob\Pokemon Go is Fun\Release is not
+: settings with an asterisk (*) are required
+:
+: Example
+: set npath=C:\Users\Bob\Release
+: works if Necrobot.exe is located at C:\Users\Bob\Release\Necrobot.exe
+:
+: Have fun botting!
+:
+:
+: ac -- *The amount of Pokemon Go accounts you will be botting
+: 1, 2, 3, 4, or 5
+set ac=
+: npath -- *Path to the FOLDER (directory) containing Necrobot.exe
+set npath=
+: npath2 -- Path to the FOLDER (directory) containing Necrobot.exe for your second account. Optional
+set npath2=
+: npath3 -- Path to the FOLDER (directory) containing Necrobot.exe for your third account. Optional
+set npath3=
+: npath4 -- Path to the FOLDER (directory) containing Necrobot.exe for your fourth account. Optional
+set npath4=
+: npath5 -- Path to the FOLDER (directory) containing Necrobot.exe for your fifth account. Optional
+set npath5=
+: ppath -- *Path to the FOLDER (directory) containing PogoLocationFeeder.GUI.exe
+set ppath=
+: qpath -- *Path to the FOLDER (directory) containing QuickSpin.exe
+set qpath=
+: sn -- Please use Y/N for sniping (Y if at least one account has a sniping config, N if not)
+: If you don't know put N
+set sn=
