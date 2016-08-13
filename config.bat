9a3de08705234b69d0f1044005ce36cf5c2390eb
Ryan <ryc9011@users.noreply.github.com>
2016-08-12 22:05:14 -0700

    Update config.bat

--- config.bat
+++ config.bat
@@ -1,34 +1,38 @@
-: Fill out the following config file by inputting your settings
-: Do NOT use quotes, Do not put a space at the beginning
-: This program currently does NOT support folders/directories with spaces in them
-: For example, C:\Users\Bob\Release is acceptable
-: and C:\Users\Bob\Pokemon Go is Fun\Release is not
-: settings with an asterisk (*) are required
-:
-: Example
-: set npath=C:\Users\Bob\Release
-: works if Necrobot.exe is located at C:\Users\Bob\Release\Necrobot.exe
-:
-: Have fun botting!
-:
-:
-: ac -- *The amount of Pokemon Go accounts you will be botting
-: 1, 2, 3, 4, or 5
+:: IMPORTANT
+:: Please read the update in readme.txt
+:: Botting is currently unsafe/not recommended at this time
+::
+:: Fill out the following config file by inputting your settings
+:: Do NOT use quotes, Do not put a space at the beginning
+:: This program currently does NOT support folders/directories with spaces in them
+:: For example, C:\Users\Bob\Release is acceptable
+:: and C:\Users\Bob\Pokemon Go is Fun\Release is not
+:: settings with an asterisk (*) are required
+::
+:: Example
+:: set npath=C:\Users\Bob\Release
+:: works if Necrobot.exe is located at C:\Users\Bob\Release\Necrobot.exe
+::
+:: Have fun botting!
+::
+::
+:: ac -- *The amount of Pokemon Go accounts you will be botting
+:: 1, 2, 3, 4, or 5
 set ac=
-: npath -- *Path to the FOLDER (directory) containing Necrobot.exe
+:: npath -- *Path to the FOLDER (directory) containing Necrobot.exe
 set npath=
-: npath2 -- Path to the FOLDER (directory) containing Necrobot.exe for your second account. Optional
+:: npath2 -- Path to the FOLDER (directory) containing Necrobot.exe for your second account. Optional
 set npath2=
-: npath3 -- Path to the FOLDER (directory) containing Necrobot.exe for your third account. Optional
+:: npath3 -- Path to the FOLDER (directory) containing Necrobot.exe for your third account. Optional
 set npath3=
-: npath4 -- Path to the FOLDER (directory) containing Necrobot.exe for your fourth account. Optional
+:: npath4 -- Path to the FOLDER (directory) containing Necrobot.exe for your fourth account. Optional
 set npath4=
-: npath5 -- Path to the FOLDER (directory) containing Necrobot.exe for your fifth account. Optional
+:: npath5 -- Path to the FOLDER (directory) containing Necrobot.exe for your fifth account. Optional
 set npath5=
-: ppath -- *Path to the FOLDER (directory) containing PogoLocationFeeder.GUI.exe
+:: ppath -- *Path to the FOLDER (directory) containing PogoLocationFeeder.GUI.exe
 set ppath=
-: qpath -- *Path to the FOLDER (directory) containing QuickSpin.exe
+:: qpath -- *Path to the FOLDER (directory) containing QuickSpin.exe
 set qpath=
-: sn -- Please use Y/N for sniping (Y if at least one account has a sniping config, N if not)
-: If you don't know put N
+:: sn -- Please use Y/N for sniping (Y if at least one account has a sniping config, N if not)
+:: If you don't know put N
 set sn=
